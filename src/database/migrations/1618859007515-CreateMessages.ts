9f76f1b6cc72c9b92a6c8db8b13e574019d81516
mateuz1234 <mat358591@gmail.com>
2021-05-10 17:49:35 -0300

    first commit

---
+++ src/database/migrations/1618859007515-CreateMessages.ts
@@ -0,0 +1,51 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateMessages1618859007515 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "messages",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            isPrimary: true,
+          },
+          {
+            name: "admin_id",
+            type: "varchar",
+            isNullable: true,
+          },
+          {
+            name: "user_id",
+            type: "varchar",
+            isNullable: true,
+          },
+          {
+            name: "text",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKUser",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("messages");
+  }
+}
